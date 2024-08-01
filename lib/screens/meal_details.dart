f5b0b03a81e52ba956b33f9757bf2cb9ab0f49e7
NTS-AkshayBokhare <akshay.bokhare@neutrinotechlabs.com>
2024-08-01 16:45:01 +0530

    used implicit animations and Explicit Animations

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -9,24 +9,64 @@ class MealDetailsScreen extends StatelessWidget {
   final void Function(Meal meal) onToggleFevorite;
 
   @override
+<<<<<<< Updated upstream
   Widget build(BuildContext context) {
+=======
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
+>>>>>>> Stashed changes
     return Scaffold(
         appBar: AppBar(
           title: Text(meal.title),
           actions: [
+<<<<<<< Updated upstream
             IconButton(onPressed: () {
               onToggleFevorite(meal);
             }, icon: const Icon(Icons.star)),
+=======
+            IconButton(
+                onPressed: () {
+                  final wasAdded = ref
+                      .read(favoriteMealsProvider.notifier)
+                      .toggleMealFavoritesStatus(meal);
+
+                  ScaffoldMessenger.of(context).clearSnackBars();
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                        wasAdded ? "Meal added as a favorite" : "meal removed"),
+                    duration: const Duration(seconds: 2),
+                  ));
+                },
+                icon: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder: (child, animation) {
+                    return RotationTransition(
+                      turns: animation,
+                      child: child,
+                    );
+                  },
+                  child: Icon(
+                    isFavorite ? Icons.star : Icons.star_border,
+                    key: ValueKey(isFavorite),
+                  ),
+                )),
+>>>>>>> Stashed changes
           ],
         ),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(
                 height: 14,
